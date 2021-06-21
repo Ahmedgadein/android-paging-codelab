7711d0a8555c706edf05a310f93869bbc8afcba4
Ahmed Gadein Ali <ahmed.g.obied@gmail.com>
2021-06-22 01:14:41 +0200

    Paging DataSource

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubPagingSource.kt
@@ -0,0 +1,49 @@
+package com.example.android.codelabs.paging.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.api.IN_QUALIFIER
+import com.example.android.codelabs.paging.model.Repo
+import retrofit2.HttpException
+import java.io.IOException
+
+private const val NETWORK_PAGE_SIZE = 50
+private const val GITHUB_STARTING_PAGE_INDEX = 1
+
+class GithubPagingSource(
+        private val service: GithubService,
+        private val query: String
+) : PagingSource<Int, Repo>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Repo> {
+        val position = params.key ?: GITHUB_STARTING_PAGE_INDEX
+        val apiQuery = query + IN_QUALIFIER
+        try {
+            val response = service.searchRepos(apiQuery, position, params.loadSize)
+            val repos = response.items
+            val nextKey = if (repos.isEmpty()) {
+                null
+            } else {
+                position + (params.loadSize / NETWORK_PAGE_SIZE)
+            }
+            return LoadResult.Page(
+                    data = repos,
+                    prevKey = if (position == GITHUB_STARTING_PAGE_INDEX) null else position - 1,
+                    nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Repo>): Int? {
+        return if (state.anchorPosition != null) {
+            return state.closestPageToPosition(state.anchorPosition!!)?.prevKey?.plus(1)
+                    ?: state.closestPageToPosition(state.anchorPosition!!)?.nextKey?.minus(1)
+        } else 1
+    }
+
+}
